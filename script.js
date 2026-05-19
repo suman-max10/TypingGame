67e688f57654613613feef31eaa056d835723131
SUMAN MANDAL <128020279+suman-max10@users.noreply.github.com>
2026-05-19 23:54:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const time = document.querySelector('.time span b');
 const mistakes = document.querySelector('.mistake span');
 
 // wpms
+
 const wpmTag = document.querySelector('.wpm span');
 const cpm = document.querySelector('.cpm span');
 const btn = document.querySelector('button');
