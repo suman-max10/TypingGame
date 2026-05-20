c25be0e19e1f159de365a1383a8b69b8c3a41176
SUMAN MANDAL <128020279+suman-max10@users.noreply.github.com>
2026-05-20 23:16:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function loadParagraph() {
   wpmTag.innerText = 0;
   cpm.innerText = 0;
 }
-
+// init typing
 function initTyping() {
   const chars = typingText.querySelectorAll('span');
   const typedChar = input.value.charAt(charIndex);
